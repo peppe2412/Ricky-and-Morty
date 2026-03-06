fcacb0b2c2780f17f2d3f22f2dcadc5d6601b6a5
peppe2412 <peppeterrazzano79@gmail.com>
2026-03-06 14:46:45 +0100

    Front-end single personaggio

--- src/pages/singleCharacter/SingleCharacter.jsx
+++ src/pages/singleCharacter/SingleCharacter.jsx
@@ -26,6 +26,26 @@ export default function SingleCharacter() {
   if (loader) return <Loader />;
   if (error) return <span>Si è verificato un errore!</span>;
 
-  return <h1>{singleChar.name}</h1>
+  return (
+    <>
+      <section className="mt-5 p-5">
+        <h1 className="lg:text-5xl text-3xl lg:text-left text-center">{singleChar.name}</h1>
+        <div className="lg:flex md:flex block gap-10 mt-10">
+          <img
+            className="w-125 shadow-sm rounded-2xl"
+            src={singleChar.image}
+            alt={singleChar.name}
+          />
+          <div className="mt-10">
+            <ul className="flex flex-col gap-3 lg:text-lg">
+              <li><strong>Gender:</strong> {singleChar.gender}</li>
+              <li><strong>Status:</strong> {singleChar.status}</li>
+              <li><strong>Origine:</strong> {singleChar.origin.name}</li>
+              <li><strong>Locazione:</strong> {singleChar.location.name}</li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
-
